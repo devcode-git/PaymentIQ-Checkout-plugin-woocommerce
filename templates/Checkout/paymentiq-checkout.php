2d8033ca7273aca72dcde5d406ae37391410957b
Håkan Sundström <sundarenius@gmail.com>
2021-08-26 15:08:49 +0200

    add calculator at checkout

--- templates/Checkout/paymentiq-checkout.php
+++ templates/Checkout/paymentiq-checkout.php
@@ -89,3 +89,62 @@ do_action( 'piq_co_wc_before_checkout_form' );
 		})
   </script>
 </form>
+
+<?php
+require_once(dirname(__FILE__) . '/../../inc/piq-co-utils.php');
+$Piq_Co_Utils = new Piq_Co_Utils();
+$calculatorWidget = $Piq_Co_Utils->getCalculatorWidget();
+$minPrice = intval($Piq_Co_Utils->getCalculatorWidgetMinPrice()); // If amount is bigger than 1990 NOK and the setting is turned on.
+?>
+
+<?php if($calculatorWidget && Piq_Co_Utils::getPiqTotalAmount() >= $minPrice) : ?>
+
+<div class="products_meta">
+  
+  <!-- PaymentIQ Developed Widget -->
+  <div id='santander-checkout-widget'></div>
+  <script>
+
+    function renderWidget () {
+      const checkout_widget = new window.SANTANDER_CHECKOUT_WIDGET(
+        'santander-checkout-widget',
+        {
+          mode: 'modern',
+          environment: 'production',
+          iframeHeight: '250px',
+					localeId: '<?php echo $Piq_Co_Utils::getSelectedLocale(); ?>',
+          country: '<?php echo $Piq_Co_Utils::getSelectedCountry(); ?>',
+          theme: {
+						raised: '<?php echo intval($Piq_Co_Utils->getCalculatorRaised()); ?>',
+            background: '<?php echo $Piq_Co_Utils->getCalculatorBackground(); ?>',
+            border: '<?php echo $Piq_Co_Utils->getCalculatorBorderColor(); ?>',
+            text: '<?php echo $Piq_Co_Utils->getCalculatorTextColor(); ?>',
+            borderRadius: '<?php echo $Piq_Co_Utils->getCalculatorBorderRadius(); ?>',
+          },
+          paymentDetails: {
+						loanAmount: <?php Piq_Co_Utils::getPiqTotalAmount(); ?>
+          }
+        }
+      );
+    }
+
+    function initWidget () {
+			console.log('initWidget')
+      if (window.SANTANDER_CHECKOUT_WIDGET) {
+        renderWidget()
+      } else {
+        setTimeout(() => {
+          initWidget()
+        }, 150);
+      }
+    }
+
+    initWidget()
+  </script>
+
+
+  <!-- END PaymentIQ Developed Widget -->
+
+</div>
+
+<?php endif; ?>
